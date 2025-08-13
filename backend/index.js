a96a2634879b2a2a39cd2791b8162aaaf6ee259d
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-13 12:19:11 +0900

    완료

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 // 프론트 연결
 const express = require("express")
-const mongoose = require("mongoose")
+const mongoose=require("mongoose")
 const cors = require("cors")
-
 const dotenv = require("dotenv")
 
 dotenv.config()
@@ -14,10 +13,17 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("mongodb 연결 성공"))
-    .catch((err) => console.log("연결 실패", err))
+        .then(()=>console.log("mongodb 연결 성공"))
+        .catch((err)=>console.log("연결 실패",err))
 
 
-app.listen(PORT, () => {
-    console.log('Hello Exress!')
+
+const postRoutes = require("./routes/PostRoutes")
+app.use("/api/posts",postRoutes)
+app.get('/',(req,res)=>{
+    res.send("Hello Express!")
+})
+
+app.listen(PORT,()=>{
+    console.log('Server is Running!')
 })
