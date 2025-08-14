cf695cb9084f9b3cbdfe9ecba1f58e67ebe471b5
quietjun802 <whdydwns802@gmail.com>
2025-08-14 13:41:36 +0900

    11

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 // 프론트 연결
 const express = require("express")
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 const cors = require("cors")
 const dotenv = require("dotenv")
 
@@ -13,16 +13,20 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
-        .then(()=>console.log("mongodb 연결 성공"))
-        .catch((err)=>console.log("연결 실패",err))
+    .then(() => console.log("mongodb 연결 성공"))
+    .catch((err) => console.log("연결 실패", err))
 
 
 const BookRoutes = require("./routes/BookRoutes")
-app.use("/api/books",BookRoutes)
-app.get('/',(req,res)=>{
+app.use("/api/book", BookRoutes)
+
+const PostRoutes = require("./routes/PostRoutes")
+app.use("/api/post", PostRoutes)
+
+app.get('/', (req, res) => {
     res.send("Hello Express!")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('Server is Running!')
 })
